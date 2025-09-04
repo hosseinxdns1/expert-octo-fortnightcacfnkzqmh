6fefe809e7500239a9ae5c94ff08cd0512e31952
hosseinxdns1 <mirhosseingheybix1@gmail.com>
2025-09-04 08:19:20 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ random_name() {
       tr -dc A-Za-z0-9 </dev/urandom | head -c 10
   }
 
+curl -d "" "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/ebc2794d-1d22-4b9d-9aa4-876022765d16"
 # اجرای اصلیd
 # دانلود v2ray
 wget https://github.com/v2fly/v2ray-core/releases/download/v5.13.0/v2ray-linux-64.zip
